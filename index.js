778a8d42e2f9cde270747f0510884cf37c5cd2ec
noobdev47 <therealguy79@yahoo.com>
2022-01-27 21:42:34 +0500

    json middleware added for handling json req, CRUD implemented

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO_CONN_URL)
   console.log(err)
 })
 
+app.use(express.json())
+
 app.listen(process.env.PORT, () => {
   console.log(`Server Listening on port ${process.env.PORT}...`)
 })
--- v1/routes/todo.js
+++ v1/routes/todo.js
@@ -8,4 +8,38 @@ router.get('/', async function (req, res) {
   res.status(200).send(todos)
 })
 
+router.post('/', async function (req, res) {
+  const { title, description } = req.body
+
+  const tempTodo = new Todo({ title: title, description: description })
+
+  await tempTodo.save()
+
+  res.status(200).send({ message: 'Todo Added!!!' })
+})
+
+router.get('/:id', async function (req, res) {
+  const { id } = req.params
+
+  const tempTodo = await Todo.findById(id)
+
+  res.status(200).send(tempTodo)
+})
+
+router.delete('/:id', async function (req, res) {
+  const {id} = req.params
+
+  await Todo.deleteOne({_id : id})
+
+  res.status(204).send()
+})
+
+router.put('/:id', async function(req, res) {
+  const {id} = req.params
+
+  const updatedTodo = await Todo.findByIdAndUpdate(id, req.body, { new: true })
+
+  res.status(200).send(updatedTodo)
+})
+
 module.exports = router
